3cbc4b9e0deb88109d65c4253610d590cde6bce3
Joel Kemp <jokemp@adobe.com>
2014-12-13 00:48:59 -0500

    Tell the user which settings are missing. Fixes #73

--- show_error.py
+++ show_error.py
@@ -1,4 +1,11 @@
 import sublime, sublime_plugin
 
-def show_error(string):
-    sublime.error_message(u'Dependents\n%s' % string)
+def show_error(string, should_show_more_info=False):
+    message = 'Dependents\n\n%s' % string
+
+    more_info = "For more info visit: https://github.com/mrjoelkemp/sublime-dependents"
+
+    if should_show_more_info:
+      message += '\n\n%s' % more_info
+
+    sublime.error_message(message)
