3da3c558551c92777134e31c9c14dbe7f96efdc0
Joel Kemp <mrjoelkemp@gmail.com>
2015-02-27 11:03:00 -0500

    Don't put a colon in the node path for Windows. Fixes #124

--- tests/lib/Test_project_settings.py
+++ tests/lib/Test_project_settings.py
@@ -1,25 +1,28 @@
 from Dependents.lib.project_settings import get_settings_from_source, get_project_settings
+from mock import Mock, patch
+import platform
+
+source = {
+    'root': 'public/assets/js',
+    'sass_root': 'public/assets/sass',
+    'config': 'public/assets/my/config.js',
+    'build_config': 'path/to/my/build.json',
+    'node_path': '/usr/bin/local',
+    'exclude': ['foo', 'bar']
+}
+
+expected = {
+    'root': 'public/assets/js/',
+    'sass_root': 'public/assets/sass/',
+    'config': 'public/assets/my/config.js',
+    'build_config': 'path/to/my/build.json',
+    # Auto adds a colon
+    'node_path': ':/usr/bin/local',
+    'exclude': ['foo', 'bar']
+}
 
 class Test(object):
     def test_get_settings_from_source(self):
-        source = {
-            'root': 'public/assets/js',
-            'sass_root': 'public/assets/sass',
-            'config': 'public/assets/my/config.js',
-            'build_config': 'path/to/my/build.json',
-            'node_path': '/usr/bin/local',
-            'exclude': ['foo', 'bar']
-        }
-
-        expected = {
-            'root': 'public/assets/js/',
-            'sass_root': 'public/assets/sass/',
-            'config': 'public/assets/my/config.js',
-            'build_config': 'path/to/my/build.json',
-            'node_path': ':/usr/bin/local',
-            'exclude': ['foo', 'bar']
-        }
-
         assert get_settings_from_source(source) == expected
 
     def test_get_settings_from_source_defaults(self):
@@ -32,5 +35,12 @@ class Test(object):
             'node_path': ''
         }
 
-    # def test_get_project_settings(self):
-    #     get_project_settings()
+    def test_node_path_windows(self):
+        example_path = 'C:\\Program Files\\nodejs'
+        platform.system = Mock(return_value='Windows')
+
+        with patch.dict(source, {'node_path': example_path}, clear=True):
+            results = get_settings_from_source(source)
+            # Should be untouched
+            assert results['node_path'] == example_path
+
