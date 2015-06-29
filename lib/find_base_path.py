494319242a04fe6ab138b63d484af23719193961
Joel Kemp <mrjoelkemp@gmail.com>
2015-06-29 16:28:17 -0400

    Support nested deprc files. Fixes #143

--- lib/find_base_path.py
+++ lib/find_base_path.py
@@ -1,5 +1,7 @@
 import sublime
+import os
 from .normalize_trailing_slash import normalize_trailing_slash
+from .printer import p
 
 def find_base_path():
     """
@@ -7,11 +9,23 @@ def find_base_path():
     to serve as the base path of the current
     """
     window = sublime.active_window()
-    filename = window.active_view().file_name()
+    view = window.active_view()
+    filename = view.file_name()
 
-    # Find the most relevant open folder
-    for folder in window.folders():
-        if folder in filename:
-            return normalize_trailing_slash(folder)
+    # The base path is the folder with the closest deprc file
+    folder_with_deprc = get_folder_with_deprc(os.path.dirname(filename));
+    p('Closest folder with a deprc:', folder_with_deprc)
 
-    return ''
+    return folder_with_deprc
+
+# TODO: Move to node tool
+def get_folder_with_deprc(baseFolder):
+    if not baseFolder:
+        return ''
+
+    deprc_file = os.path.join(baseFolder, '.deprc')
+
+    if os.path.exists(deprc_file):
+        return baseFolder
+    else:
+        return get_folder_with_deprc(os.path.dirname(baseFolder))
