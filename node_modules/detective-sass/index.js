af9af22f563f4dc6e81fda5ef57ae5afe5ab16f0
Joel Kemp <mrjoelkemp@gmail.com>
2016-06-19 13:48:18 -0400

    Bump backend for SASS fixes via sass ast generation
    Ref https://github.com/mrjoelkemp/node-detective-sass/pull/3
    Ref https://github.com/mrjoelkemp/node-dependents-editor-backend/pull/56

--- node_modules/detective-sass/index.js
+++ node_modules/detective-sass/index.js
@@ -1,3 +1,6 @@
+var Walker = require('node-source-walk');
+var sass = require('sass-thematic');
+
 /**
  * Extract the @import statements from a given sass file's content
  *
@@ -9,18 +12,43 @@ module.exports = function(fileContent) {
   if (typeof fileContent !== 'string') { throw new Error('content is not a string'); }
 
   var dependencies = [];
-  var importRegex = /\@import\s['"](.*)['"]/g;
 
-  var matches;
+  var walker = new Walker();
 
-  do {
-    matches = importRegex.exec(fileContent);
+  var ast = sass.parseASTSync({
+    data: fileContent
+  });
 
-    if (matches) {
-      dependencies.push(matches[1]);
-    }
+  walker.walk(ast, function(node) {
+    if (!isImportStatement(node)) { return; }
 
-  } while (matches);
+    dependencies = dependencies.concat(extractDependencies(node));
+  });
 
   return dependencies;
 };
+
+function isImportStatement(node) {
+  if (!node || node.type !== 'atrules') { return false; }
+  if (!node.content.length || node.content[0].type !== 'atkeyword') { return false; }
+
+  var atKeyword = node.content[0];
+
+  if (!atKeyword.content.length) { return false; }
+
+  var importKeyword = atKeyword.content[0];
+
+  if (importKeyword.type !== 'ident' || importKeyword.content !== 'import') { return false; }
+
+  return true;
+}
+
+function extractDependencies(importStatementNode) {
+  return importStatementNode.content
+  .filter(function(innerNode) {
+    return innerNode.type === 'string' || innerNode.type === 'ident';
+  })
+  .map(function(identifierNode) {
+    return identifierNode.content.replace(/["']/g, '');
+  });
+}
