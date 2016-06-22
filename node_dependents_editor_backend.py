142a2b5d3dd0d8d55c6cab904eb2c5bb2b0fed53
Joel Kemp <mrjoelkemp@gmail.com>
2016-06-21 22:14:46 -0400

    Analytics: Track editor version

--- node_dependents_editor_backend.py
+++ node_dependents_editor_backend.py
@@ -2,7 +2,9 @@ import sys
 import sublime
 import traceback
 
-if sys.version_info < (3,):
+is_ST2 = sys.version_info < (3,)
+
+if is_ST2:
     from lib.show_error import show_error
     from node_bridge import node_bridge
     from lib.track import t
@@ -16,8 +18,16 @@ else:
     from .lib.project_settings import get_project_settings
 
 def backend(options):
+    args = []
+
     if 'filename' in options:
-        args = ['--filename=' + options['filename']]
+        args.append('--filename=' + options['filename'])
+
+    if is_ST2:
+        editor = 'ST2'
+    else:
+        editor = 'ST3'
+    args.append('--editor=' + editor)
 
     if 'command' in options:
         args.append('--' + options['command'])
@@ -28,7 +38,7 @@ def backend(options):
     try:
         node_path = get_project_settings(options['filename']).get('node_path')
         p('node_path from settings: ', node_path)
-        
+
         # TODO: It's not great to hardcode the Package name here but __file__ isn't consistent between Python 2 and 3
         bin_path = sublime.packages_path() + '/Dependents/node_modules/dependents-editor-backend/bin/cli.js'
         p('bin path', bin_path)
